08f262b5c3c3156fc56f98247e07c02f87e4ec66
GoldFiish <komodo_1100@nate.com>
2016-07-13 06:25:08 +0900

    remove img
    remove img

--- Nebula Kalista/Program.cs
+++ Nebula Kalista/Program.cs
@@ -9,8 +9,8 @@ namespace NebulaKalista
 {
     class Program
     {
-        private static readonly TextureLoader TextureLoader = new TextureLoader();
-        private static Sprite TopSprite { get; set; }
+        //private static readonly TextureLoader TextureLoader = new TextureLoader();
+        //private static Sprite TopSprite { get; set; }
 
         private static void Main(string[] args)
         {
@@ -21,17 +21,17 @@ namespace NebulaKalista
         {
             Kalista.Load();
 
-            TextureLoader.Load("top", Properties.Resources.KalistaLogo);
-            TopSprite = new Sprite(() => TextureLoader["top"]);
+            //TextureLoader.Load("top", Properties.Resources.KalistaLogo);
+            //TopSprite = new Sprite(() => TextureLoader["top"]);
 
-            Drawing.OnEndScene += Drawing_OnEndScene;
+            //Drawing.OnEndScene += Drawing_OnEndScene;
         }           
 
-        public static void Drawing_OnEndScene(EventArgs args)
-        {
-            TopSprite.Draw(new Vector2((Drawing.Width - 600) / 2, 30));
+        //public static void Drawing_OnEndScene(EventArgs args)
+        //{
+        //    TopSprite.Draw(new Vector2((Drawing.Width - 600) / 2, 30));
 
-            Core.DelayAction(() => TextureLoader.Unload("top"), 12000);
-        }
+        //    Core.DelayAction(() => TextureLoader.Unload("top"), 12000);
+        //}
     }
 }
