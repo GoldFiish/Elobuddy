0397a20622e61e36c18eb19df712cf3b5f217083
GoldFiish <komodo_1100@nate.com>
2017-01-13 21:20:09 +0900

    7.1.0.0 Beta
    7.1.0.0 Beta

---
+++ Nebula TwistedFate/ControllN/Eng.cs
@@ -0,0 +1,95 @@
+﻿namespace NebulaTwistedFate.ControllN
+{
+    class Eng : Controller
+    {
+        public Eng()
+        {
+            Dictionary.Add(EnumContext.SelectLanguage,  "Press F5 after select language.");
+            Dictionary.Add(EnumContext.Text_1,          "If you like this addon, hope Upvote click or donation.");
+            Dictionary.Add(EnumContext.Text_2,          "Donated amount will be used for the making of better addon.");
+           
+            Dictionary.Add(EnumContext.Combo,           "- Combo");
+            Dictionary.Add(EnumContext.ComboQMode,      "[ Q ] Option");
+            Dictionary.Add(EnumContext.ComboQMode0,     "[ Q ] Activation");
+            Dictionary.Add(EnumContext.ComboQMode1,     "Enemy has stun Buff");
+            Dictionary.Add(EnumContext.EnemyNum0,       "Red card - Enemies around the target");
+            Dictionary.Add(EnumContext.EnemyNum1,       "");
+
+            Dictionary.Add(EnumContext.Harass,          "- Harass");
+            
+            Dictionary.Add(EnumContext.Clear,           "- Lane / Jungle");
+            Dictionary.Add(EnumContext.LaneClear,       "Lane");
+            Dictionary.Add(EnumContext.LaneMode,        "[ Q ] Option");
+            Dictionary.Add(EnumContext.LaneMode0,       "Auto");
+            Dictionary.Add(EnumContext.LaneMode1,       "Fast push");
+            Dictionary.Add(EnumContext.LaneMode2,       "Minions Kill");
+            Dictionary.Add(EnumContext.MinionsNum,      "Minions Num - ");
+            Dictionary.Add(EnumContext.LaneCardRed,     "Red card Use");
+            Dictionary.Add(EnumContext.MinionsKNum,     "Minions Kill Num - ");
+            Dictionary.Add(EnumContext.LaneCardBlue,    "Blue card Use");
+            
+            Dictionary.Add(EnumContext.JungleClear,     "Jungle");
+
+            Dictionary.Add(EnumContext.Item,            "- Item");
+            Dictionary.Add(EnumContext.ItemExp,         "Auto - Bilgewater, Youmuu, Hextech Gunblade");
+            Dictionary.Add(EnumContext.sBlade,          "Blade of the Ruined King");
+            Dictionary.Add(EnumContext.sBKHp1,          "If player hp is lower than ");
+            Dictionary.Add(EnumContext.sBKHp2,          " [ Blade of the Ruined King ] is used");
+            Dictionary.Add(EnumContext.sQsilver,        "Quicksilver Sash");
+            Dictionary.Add(EnumContext.sScimitar,       "Mercurial Scimitar");
+            Dictionary.Add(EnumContext.Delay,           "Cast delay");
+            Dictionary.Add(EnumContext.sBuffType,       "Buff Type");
+            Dictionary.Add(EnumContext.sPoisons,        "Poisons");
+            Dictionary.Add(EnumContext.sSupression,     "Supression");
+            Dictionary.Add(EnumContext.sBlind,          "Blind");
+            Dictionary.Add(EnumContext.sCharm,          "Charm");
+            Dictionary.Add(EnumContext.sFear,           "Fear");
+            Dictionary.Add(EnumContext.sPolymorph,      "Polymorph");
+            Dictionary.Add(EnumContext.sSilence,        "Silence");
+            Dictionary.Add(EnumContext.sSlow,           "Slow");
+            Dictionary.Add(EnumContext.sSnare,          "Snare");
+            Dictionary.Add(EnumContext.sStun,           "Stun");
+            Dictionary.Add(EnumContext.sKnockup,        "Knockup");
+            Dictionary.Add(EnumContext.sTaunt,          "Taunt");
+            Dictionary.Add(EnumContext.sZhonya,         "Zhonyas Hourglass");
+            Dictionary.Add(EnumContext.sZhonyaBHp,      "If player hp is lower than ");           
+            Dictionary.Add(EnumContext.sZhonyaBDmg,     "Instantaneous AA damage > ");
+            Dictionary.Add(EnumContext.sZhonyaSHp,      "If player hp is lower than ");
+            Dictionary.Add(EnumContext.sZhonyaSDmg,     "Instantaneous skill damage > ");
+            Dictionary.Add(EnumContext.sZhonyaR,        "Choose a dangerous [ R ]");
+
+            Dictionary.Add(EnumContext.Msic,            "- Msic");
+            Dictionary.Add(EnumContext.AutoIgnite,      "Auto Ignite");
+            Dictionary.Add(EnumContext.KillSteal,       "Kill steal");
+            Dictionary.Add(EnumContext.JungSteal,       "Jungle steal");
+            Dictionary.Add(EnumContext.AutoQ,           "Auto [ Q ] - Stun, Supression, Snare, Charm, Recall");
+            Dictionary.Add(EnumContext.AutoPickYel,     "Auto Yellow card pick - Use R");
+            Dictionary.Add(EnumContext.PickDelay,       "Card pick dealy");
+            Dictionary.Add(EnumContext.DisAA,           "Disable Auto Attack during loop cycle");
+            Dictionary.Add(EnumContext.InterruptLv,     "Danger");
+            Dictionary.Add(EnumContext.InterruptLv1,    "Medium");
+            Dictionary.Add(EnumContext.InterruptLv2,    "High");
+
+            Dictionary.Add(EnumContext.Draw,            "- Draw");
+            Dictionary.Add(EnumContext.DrawQ,           "[ Q ] range");
+            Dictionary.Add(EnumContext.DrawW,           "[ W ] range");
+            Dictionary.Add(EnumContext.DrawR,           "[ R ] range");
+            Dictionary.Add(EnumContext.DrawText,        "Killable / Damage %");
+            Dictionary.Add(EnumContext.SpellQ,          "[ Q ] Use");
+            Dictionary.Add(EnumContext.SpellW,          "[ W ] Use");
+            Dictionary.Add(EnumContext.SpellR,          "[ R ] Use");
+
+            Dictionary.Add(EnumContext.QPrediction,     "Hit percent");
+            Dictionary.Add(EnumContext.CardMode,        "[ W ] Option");
+            Dictionary.Add(EnumContext.CardMode0,       "Auto");
+            Dictionary.Add(EnumContext.CardMode1,       "Red card");
+            Dictionary.Add(EnumContext.CardMode2,       "Yellow card");
+
+            Dictionary.Add(EnumContext.ManaStatus,      "If mana is higher than ");
+            Dictionary.Add(EnumContext.BlueStatus,      " used blue card ");
+
+            Dictionary.Add(EnumContext.sMore,           "");
+            Dictionary.Add(EnumContext.sLow,            "");
+        }
+    }
+}
---
+++ Nebula TwistedFate/ControllN/Kor.cs
@@ -0,0 +1,95 @@
+﻿namespace NebulaTwistedFate.ControllN
+{
+    class Kor : Controller
+    {
+        public Kor()
+        {
+            Dictionary.Add(EnumContext.SelectLanguage,  "언어 선택 후 F5를 눌러주세요.");
+            Dictionary.Add(EnumContext.Text_1,          "이 애드온이 마음에 드신다면, 추천 또는 페이팔 기부를 부탁드리며,");
+            Dictionary.Add(EnumContext.Text_2,          "기부하신 금액은 애드온 제작에 사용됩니다. 감사합니다.");
+           
+            Dictionary.Add(EnumContext.Combo,           "- 콤보");
+            Dictionary.Add(EnumContext.ComboQMode,      "[ Q ] 옵션");
+            Dictionary.Add(EnumContext.ComboQMode0,     "아무때나");
+            Dictionary.Add(EnumContext.ComboQMode1,     "스턴 상태");
+            Dictionary.Add(EnumContext.EnemyNum0,       "빨강 카드 - 타켓 주위에 적이 ");
+            Dictionary.Add(EnumContext.EnemyNum1,       " 명 이상");
+
+            Dictionary.Add(EnumContext.Harass,          "- 견제");
+            
+            Dictionary.Add(EnumContext.Clear,           "- 라인 / 정글");
+            Dictionary.Add(EnumContext.LaneClear,       "라인");
+            Dictionary.Add(EnumContext.LaneMode,        "[ Q ] 옵션");
+            Dictionary.Add(EnumContext.LaneMode0,       "자동");
+            Dictionary.Add(EnumContext.LaneMode1,       "빠르게 밀기");
+            Dictionary.Add(EnumContext.LaneMode2,       "미니언 킬");
+            Dictionary.Add(EnumContext.MinionsNum,      "미니언 개수 - ");
+            Dictionary.Add(EnumContext.LaneCardRed,     "빨강 카드 사용");
+            Dictionary.Add(EnumContext.MinionsKNum,     "미니언 킬 개수 - ");
+            Dictionary.Add(EnumContext.LaneCardBlue,    "파랑 카드 사용");
+            
+            Dictionary.Add(EnumContext.JungleClear,     "정글");
+
+            Dictionary.Add(EnumContext.Item,            "- 아이템");
+            Dictionary.Add(EnumContext.ItemExp,         "자동 - 빌지워터, 요우무, 마법공학 총검");
+            Dictionary.Add(EnumContext.sBlade,          "몰락한 왕의 검");
+            Dictionary.Add(EnumContext.sBKHp1,          "적 체력이 ");
+            Dictionary.Add(EnumContext.sBKHp2,          " 이하일때 몰왕 사용");
+            Dictionary.Add(EnumContext.sQsilver,        "수은 장식띠");
+            Dictionary.Add(EnumContext.sScimitar,       "헤르매스의 시미터");
+            Dictionary.Add(EnumContext.Delay,           "캐스팅 속도");
+            Dictionary.Add(EnumContext.sBuffType,       "버프 타입");
+            Dictionary.Add(EnumContext.sPoisons,        "독");
+            Dictionary.Add(EnumContext.sSupression,     "제압");
+            Dictionary.Add(EnumContext.sBlind,          "실명");
+            Dictionary.Add(EnumContext.sCharm,          "매혹");
+            Dictionary.Add(EnumContext.sFear,           "공포");
+            Dictionary.Add(EnumContext.sPolymorph,      "변형");
+            Dictionary.Add(EnumContext.sSilence,        "침묵");
+            Dictionary.Add(EnumContext.sSlow,           "둔화");
+            Dictionary.Add(EnumContext.sSnare,          "덫");
+            Dictionary.Add(EnumContext.sStun,           "기절");
+            Dictionary.Add(EnumContext.sKnockup,        "에어본");
+            Dictionary.Add(EnumContext.sTaunt,          "도발");
+            Dictionary.Add(EnumContext.sZhonya,         "존야의 모래시계");
+            Dictionary.Add(EnumContext.sZhonyaBHp,      "사용자의 체력이 ");           
+            Dictionary.Add(EnumContext.sZhonyaBDmg,     "순간 평타 데미지가 ");
+            Dictionary.Add(EnumContext.sZhonyaSHp,      "사용자의 체력이 ");
+            Dictionary.Add(EnumContext.sZhonyaSDmg,     "순간 스킬 데미지가 ");
+            Dictionary.Add(EnumContext.sZhonyaR,        "위험한 궁극기");
+
+            Dictionary.Add(EnumContext.Msic,            "- 기타");
+            Dictionary.Add(EnumContext.AutoIgnite,      "자동 점화 사용");
+            Dictionary.Add(EnumContext.KillSteal,       "킬 스틸 사용");
+            Dictionary.Add(EnumContext.JungSteal,       "정글 스틸 사용");
+            Dictionary.Add(EnumContext.AutoQ,           "자동 [ Q ] - 스턴, 속박, 제압, 매혹, 귀환");
+            Dictionary.Add(EnumContext.AutoPickYel,     "궁극기 사용시 자동 픽 - 노랑 카드");
+            Dictionary.Add(EnumContext.PickDelay,       "카드 픽 딜레이");
+            Dictionary.Add(EnumContext.DisAA,           "카드 선택 시 자동 공격 금지");
+            Dictionary.Add(EnumContext.InterruptLv,     "위험도");
+            Dictionary.Add(EnumContext.InterruptLv1,    "보통");
+            Dictionary.Add(EnumContext.InterruptLv2,    "높음");
+
+            Dictionary.Add(EnumContext.Draw,            "- 시각효과");
+            Dictionary.Add(EnumContext.DrawQ,           "Q 범위");
+            Dictionary.Add(EnumContext.DrawW,           "W 범위");
+            Dictionary.Add(EnumContext.DrawR,           "R 범위");
+            Dictionary.Add(EnumContext.DrawText,        "킬각 및 데미지 %");
+            Dictionary.Add(EnumContext.SpellQ,          "Q 사용");
+            Dictionary.Add(EnumContext.SpellW,          "W 사용");
+            Dictionary.Add(EnumContext.SpellR,          "R 사용");
+
+            Dictionary.Add(EnumContext.QPrediction,     "정확도");
+            Dictionary.Add(EnumContext.CardMode,        "[ W ] 옵션");
+            Dictionary.Add(EnumContext.CardMode0,       "자동");
+            Dictionary.Add(EnumContext.CardMode1,       "빨강 카드");
+            Dictionary.Add(EnumContext.CardMode2,       "노랑 카드");
+
+            Dictionary.Add(EnumContext.ManaStatus,      "보유한 마나가 ");
+            Dictionary.Add(EnumContext.BlueStatus,      "이하일 때 파랑카드 사용 ");
+
+            Dictionary.Add(EnumContext.sMore,           " 이상일 때");
+            Dictionary.Add(EnumContext.sLow,            " 이하일 때");
+        }
+    }
+}
